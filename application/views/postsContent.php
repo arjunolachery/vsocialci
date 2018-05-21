f8f03e6a70899021a5161a8b2926428ae8539196
Arjun Olachery <arjun2olachery@gmail.com>
2018-05-21 13:53:09 +0530

    minor edits

--- application/views/postsContent.php
+++ application/views/postsContent.php
@@ -3,15 +3,39 @@
     <div class="col-lg-6">
       <br>
       <h3 style="display:inline">Posts from <span id="circleDefinition">everyone</span>&nbsp;<button class="sideButton" id="circleChangeButton"><img src="../../assets/images/levels.png"></button></h3><br>
-      <div id="circleChangeContent"><ul><li>asas</li><li>asas</li><li>asas</li></ul><br>
+      <div id="circleChangeContent"><ul><li>asas</li><li>asas</li><li>asas</li></ul>
       <center><button class="sideButton" id="circleChangeClose"><img src="../../assets/images/error.png"></button></center>
     </div>
-      <?php if(isset($_SESSION['success']))
-      {
-        ?><div class="alert alert-success"><?php echo $_SESSION['success'];?></div>
+      <br>
 
-      <?php } ?>
-      Hello, <?php echo $_SESSION['uid']; ?>
+
+      <div class="row fromto">
+      <div class="col-1"></div>
+      <div class="col-10">To Arjun,</div>
+      <div class="col-1"></div>
+      </div>
+
+      <div class="row">
+      <div class="col-1"></div>
+      <div class="col-10 textareadivclass"><textarea class="droplinetext" width="100%" name="droplinetext" id="textareamail" placeholder="Type here to drop a line." cols="100%" onclick="openMail()"></textarea></input></div>
+      <div class="col-1"></div>
+      </div>
+
+      <div class="row fromto">
+      <div class="col-1"></div>
+      <div class="col-10">From,<br>arjun<br><input type="button" class="fromtobutton" value="send >"></div>
+      <div class="col-1"></div>
+      </div>
+
+      <div class="row">
+      <div class="col-1"></div>
+      <div class="col-10 fromtoend"></div>
+      <div class="col-1"></div>
+      </div>
+
+
+
+      <p><input type="text" id="postsBar" placeholder="Type here to post"></p>
     </div>
     <div class="col-lg-3"><br><h3 style="display:inline">Online<button class="sideButton"><img src="../../assets/images/levels.png"></button></h3></div>
 </div>
@@ -33,4 +57,74 @@ $("#circleChangeButton").click(function(){
 $("#circleChangeClose").click(function(){
   $("#circleChangeContent").hide();
 });
+
+$("#postsBar").keyup(function(){
+  alert('hi');
+});
+
+//code for textarea
+$('textarea').each(function () {
+  this.setAttribute('style', 'height:' + (this.scrollHeight) + 'px;overflow-y:hidden;');
+}).on('input', function () {
+  this.style.height = 'auto';
+  this.style.height = (this.scrollHeight) + 'px';
+});
+
+  $(".fromto").hide();
+  var count=0;
+$("#textareamail").keypress(function(){
+  if(count==0)
+  {
+    $("#textareamail").css("text-align","left");
+    $(".textareadivclass").css("text-align","left");
+    $(".fromto").fadeIn("slow");
+    count++;
+  }
+  //alert("hi");
+});
+
+/*
+
+    $(".fromtobutton").click(function(){
+      $.fn.ajaxSubmitFunction();
+    });
+
+    $.fn.ajaxSubmitFunction = function(){
+      var message = $("#textareamail").val();
+      var urlmessage = window.location.href;
+      // Returns successful data submission message when the entered information is stored in database.
+      var dataString = 'message='+ message +'&urlmessage='+urlmessage;
+      if(message=='')
+      {
+      alert("Please Fill the Field");
+      }
+      else
+      {
+        //alert('hello');
+      // AJAX Code To Submit Form.
+      $.ajax({
+      type: "POST",
+      url: "entryProcessMessage.php",
+      data: dataString,
+      cache: false,
+      success: function(result){
+      //alert(result);
+      if(result=='1')
+      {
+        //$(".fromtoend").html("Thanks!");
+        $('#textareamail').prop('readonly', true);
+        $("#textareamail").val("Thanks!");
+        $("#textareamail").css("text-align","center");
+        $(".textareadivclass").css("text-align","center");
+        $(".fromto").hide();
+
+      }
+      else {
+        $('.fromtoend').html("Error"+result);
+      }
+      }
+      });
+      }
+    };
+*/
 </script>
