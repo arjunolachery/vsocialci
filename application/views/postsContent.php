1afeafe1a6d9256ea23f7969e48cf8fa20bf88b6
Arjun Olachery <arjun2olachery@gmail.com>
2018-05-21 14:49:20 +0530

    front end posts update

--- application/views/postsContent.php
+++ application/views/postsContent.php
@@ -10,32 +10,21 @@
 
 
       <div class="row fromto">
-      <div class="col-1"></div>
-      <div class="col-10">To Arjun,</div>
-      <div class="col-1"></div>
+      <div class="col-lg-12"><!--To Everyone,--></div>
       </div>
 
       <div class="row">
-      <div class="col-1"></div>
-      <div class="col-10 textareadivclass"><textarea class="droplinetext" width="100%" name="droplinetext" id="textareamail" placeholder="Type here to drop a line." cols="100%" onclick="openMail()"></textarea></input></div>
-      <div class="col-1"></div>
+      <div class="col-lg-12 textareadivclass"><textarea class="droplinetext" width="100%" name="droplinetext" id="textareamail" placeholder="Type here to post." cols="100%" onclick="openMail()"></textarea></input></div>
       </div>
 
       <div class="row fromto">
-      <div class="col-1"></div>
-      <div class="col-10">From,<br>arjun<br><input type="button" class="fromtobutton" value="send >"></div>
-      <div class="col-1"></div>
+      <div class="col-lg-12"><!--From,<br>arjun--><br><button class="sideButton" id="postConfirm"><img src="../../assets/images/check.png"></button><button class="sideButton" id="postClose"><img src="../../assets/images/error.png"></button></div>
       </div>
 
       <div class="row">
-      <div class="col-1"></div>
-      <div class="col-10 fromtoend"></div>
-      <div class="col-1"></div>
+      <div class="col-12 fromtoend"></div>
       </div>
 
-
-
-      <p><input type="text" id="postsBar" placeholder="Type here to post"></p>
     </div>
     <div class="col-lg-3"><br><h3 style="display:inline">Online<button class="sideButton"><img src="../../assets/images/levels.png"></button></h3></div>
 </div>
@@ -82,7 +71,42 @@ $("#textareamail").keypress(function(){
   }
   //alert("hi");
 });
+$("#postClose").click(function(){
+  count=0;
+  $(".fromto").hide();
+  $("#textareamail").hide();
+  $("#textareamail").val('');
+  $("#textareamail").css("text-align","center");
+  $(".textareadivclass").css("text-align","center");
+  $("#textareamail").fadeIn();
+});
 
+$("#postConfirm").click(function(){
+  var data=$("#textareamail").val();
+  $.fn.postData(data);
+  //alert(data);
+  count=0;
+  $(".fromto").hide();
+  $("#textareamail").hide();
+  $("#textareamail").css("color","green");
+  $("#textareamail").val('Successfully Posted');
+  $("#textareamail").css("text-align","center");
+  $(".textareadivclass").css("text-align","center");
+  $("#textareamail").fadeIn();
+  function show_popup(){
+    $("#textareamail").hide();
+    $("#textareamail").css("color","black");
+    $("#textareamail").val('');
+    $("#textareamail").fadeIn();
+   };
+   window.setTimeout( show_popup, 1500 );
+
+
+});
+
+$.fn.postData=function(data){
+  alert(data);
+}
 /*
 
     $(".fromtobutton").click(function(){
