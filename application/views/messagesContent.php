56ac7ec3efca108c90eb7349c9577dc7a5e14e1f
arjunolachery <arjun2olachery@gmail.com>
2018-05-21 07:20:22 +0530

    front-end finished

---
+++ application/views/messagesContent.php
@@ -0,0 +1,14 @@
+<div class="row" id="settingsContent">
+  <div class="col-lg-3"></div>
+    <div class="col-lg-6">
+      <h3>Messages</h3>
+      <h4>Here are your messages</h4>
+      <center><button class="sideButton" id="buttonClose"><img src="../../assets/images/error.png"></button></center>
+    </div>
+    <div class="col-lg-3"></div>
+</div>
+<script>
+$("#buttonClose").click(function(){
+$.fn.openContent(4);
+});
+</script>
--- application/views/profile.php
+++ application/views/profile.php
@@ -1,27 +1,15 @@
 <!DOCTYPE html>
 <html lang="en">
   <head>
+    <title>Login Page</title>
     <meta charset="utf-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
-    <title>Login Page</title>
-
-    <!-- Bootstrap -->
     <link href="<?php echo base_url();?>assets/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" type="text/css" href="<? echo base_url();?>/assets/css/profile.css">
     <link href="https://fonts.googleapis.com/css?family=Raleway" rel="stylesheet">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-
-    <!-- HTML5 shim and Respond.js for IE8 support of HTML5 elements and media queries -->
-    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
-    <!--[if lt IE 9]>
-      <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
-      <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
-    <![endif]-->
-    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-    <!-- Include all compiled plugins (below), or include individual files as needed -->
     <script src="<?php echo base_url();?>assets/js/bootstrap.min.js"></script>
     <script src="<?php echo base_url();?>assets/js/profile.js"></script>
   </head>
@@ -45,7 +33,6 @@
 
 <div id="mainContent">
 </div>
-
 
 
 
