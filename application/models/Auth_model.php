66297177864a398e73d016641b3e68e9492066f9
arjunolachery <arjun2olachery@gmail.com>
2018-05-21 08:25:29 +0530

    added models to make ci app efficient

--- application/models/Auth_model.php
+++ application/models/Auth_model.php
@@ -1,9 +1,35 @@
 <?php
-class Auth_model extends CI_model{
-
-  public function __construct(){
-    parent::__construct();
-  }
-
+class Auth_model extends CI_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    public function retrieveUserPrimaryDetails($email, $password)
+    {
+        $this->db->select('*');
+        $this->db->from('users');
+        $this->db->where(array('email'=>$email,'password'=>$password));
+        $query=$this->db->get();
+        $user=$query->row();
+        return $user;
+    }
+    public function checkUserExist($email)
+    {
+      $this->db->select('*');
+      $this->db->from('users');
+      $this->db->where(array('email'=>$email));
+      $query=$this->db->get();
+      $user=$query->row();
+      return $user->email;
+    }
+    public function checkUserExistId($uid)
+    {
+      $this->db->select('*');
+      $this->db->from('users');
+      $this->db->where(array('user_id'=>$uid));
+      $query=$this->db->get();
+      $user=$query->row();
+      return $user->activationKey;
+    }
 }
- ?>
---
+++ application/models/User_model.php
@@ -0,0 +1,43 @@
+<?php
+class User_model extends CI_Model
+{
+    public function __construct()
+    {
+        parent::__construct();
+    }
+    public function checkActivationStatus($uid)
+    {
+      $this->db->select('*');
+      $this->db->from('users');
+      $this->db->where(array('user_id'=>$uid));
+      $query=$this->db->get();
+      $user=$query->row();
+      return $user->activation;
+    }
+    /*
+    public function messagesConf($id)
+    {
+      switch($id)
+      {
+        case 1:
+        return "You have successfully signed up. Proceed to log in.";
+        case 2:
+        return "You have successfully logged out";
+        case default:
+        break;
+      }
+    }
+    */
+    /*
+    public function messagesRej($id)
+    {
+      switch($id)
+      {
+        case 1:
+        return "You have successfully signed up. Proceed to log in.";
+        case default:
+        break;
+      }
+    }
+    */
+}
